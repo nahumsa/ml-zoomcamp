8071804c4f4a35448731ac0af1fd24ad2e3e4346
Nahum Sá <nahumsaa@gmail.com>
2022-10-03 17:37:53 -0300

    add hw-04

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd data/
 wget https://raw.githubusercontent.com/alexeygrigorev/datasets/master/housing.csv
+wget https://raw.githubusercontent.com/alexeygrigorev/datasets/master/AER_credit_card_data.csv
